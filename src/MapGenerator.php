5dae02ff453f2972a6af78e5099b2c24839f93b5
Javan Eskander <javan@steppinghat.com>
2021-03-18 16:31:56 +1100

    Updated the upstream URL for the generator

--- src/MapGenerator.php
+++ src/MapGenerator.php
@@ -4,12 +4,14 @@ namespace SteppingHat\EmojiDetector;
 
 class MapGenerator {
 
+    private static $upstreamUrl = 'https://unpkg.com/emoji.json@13.1.0/emoji.json';
+
 	public static function generateMap() {
 		$dataDir = __DIR__."/../var";
 		if(!is_dir($dataDir)) mkdir($dataDir, 0777, true);
 
 		// Load emoji data from amio/emoji.json
-		$emojiData = json_decode(file_get_contents('https://unpkg.com/emoji.json@13.1.0/emoji.json'), true);
+		$emojiData = json_decode(file_get_contents(self::$upstreamUrl), true);
 
 		$mapData = [];
 		$rawEmojis = [];
